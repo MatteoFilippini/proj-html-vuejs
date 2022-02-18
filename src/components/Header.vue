277c5b3f17b89da6230972c51d73ddb287ec7fe1
MatteoFilippini <matteofilippini8@gmail.com>
2022-02-18 16:50:02 +0100

    added bg-image on header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,20 @@
 <template>
   <header>
-    <Navbar :items="itemsNavbar" />
+    <!-- <div class="container">
+      <div class="row">
+        <div class="col"></div>
+      </div>
+    </div> -->
+    <div class="topHeader">
+      <Navbar :items="itemsNavbar" />
+      <img src="../assets/img/dark-logo.png" alt="logo" />
+      <div class="info">
+        <i class="fa-solid fa-cart-shopping"></i>
+        <i class="fa-regular fa-circle-user"></i>
+        <input type="text" placeholder="Search" />
+      </div>
+    </div>
+    <div class="bottomHeader">a</div>
   </header>
 </template>
 
@@ -8,6 +22,9 @@
 import Navbar from "./Navbar.vue";
 export default {
   name: "Header",
+  data() {
+    return {};
+  },
   props: ["itemsNavbar"],
   components: {
     Navbar,
@@ -16,4 +33,27 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+// top header
+.topHeader {
+  display: flex;
+  max-height: 70px;
+  justify-content: space-around;
+  align-items: center;
+  img {
+    height: 30px;
+  }
+  .info {
+    i {
+      padding: 0 7px;
+      font-size: 16px;
+    }
+  }
+}
+// bottom header
+.bottomHeader {
+  height: 800px;
+  background-image: url("../assets/img/home-3-hero-bg.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+}
 </style>
